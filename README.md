83f6251519b5c728134474b10014736d6224ec5f
Gabriel <gabrielpereira050201@hotmail.com>
2022-07-07 19:01:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform Github Pipeline
+
+Exemplo de como subir uma instancia EC2 da AWS usando a pipeline do Github actions
---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.17.1"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+/*
+  Necessario configurar de acordo com as credenciais da AWS
+*/
+provider "aws" {
+  profile = "terraform_profile"
+  region = "us-east-1"
+}
+
+/*
+  Cria a instancia do EC2
+*/
+resource "aws_instance" "app" {
+  ami = "ami-0729e439b6769d6ab"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-app"
+  }
+}
