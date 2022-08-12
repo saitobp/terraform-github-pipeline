c8a6907e81a07d70d25ecb126b23a522d5efc753
Gabriel <gabrielpereira050201@hotmail.com>
2022-08-11 22:24:24 -0300

    Fixed [RDS Module] Instance class estava incorreta

--- terraform/modules/rds-module/main.tf
+++ terraform/modules/rds-module/main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "database" {
   allocated_storage = 20
   engine = "postgres"
   engine_version = "13.4"
-  instance_class = "db.t2.micro"
+  instance_class = "db.t3.micro"
   db_name = var.db_name
   username = var.db_username
   password = var.db_password
